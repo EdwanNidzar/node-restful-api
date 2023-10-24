22dbc2a561253ea7abf57786b8be96db4adf1767
EdwanNidzar <56621669+EdwanNidzar@users.noreply.github.com>
2023-10-24 20:38:00 +0800

    feat : contacts api
    create, get, update, remove, search.

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,12 +1,21 @@
 import express from "express";
 import userController from "../controllers/user-controller.js";
+import contactController from "../controllers/contact-controller.js";
 import { authMiddleware } from "../middleware/auth-middleware.js";
 
 const userRouter = new express.Router();
-
 userRouter.use(authMiddleware);
+
+// USER API
 userRouter.get("/api/users/current", userController.get);
 userRouter.patch("/api/users/current", userController.update);
 userRouter.delete("/api/users/logout", userController.logout);
+
+// CONTACT API
+userRouter.post("/api/contacts", contactController.create);
+userRouter.get("/api/contacts/:contactId", contactController.get);
+userRouter.put("/api/contacts/:contactId", contactController.update);
+userRouter.delete("/api/contacts/:contactId", contactController.remove);
+userRouter.get("/api/contacts", contactController.search);
 
 export { userRouter };
--- test/user.test.js
+++ test/user.test.js
@@ -1,7 +1,6 @@
 import supertest from "supertest";
 import { web } from "../src/application/web.js";
-import { logger } from "../src/application/logging.js";
-import { createTestUser, getTestUser, removeTestUser } from "./test.util.js";
+import { createTestUser, getTestUser, removeTestUser } from "./test-util.js";
 import bcrypt from "bcrypt";
 
 describe("POST /api/users", function () {
@@ -12,11 +11,9 @@ describe("POST /api/users", function () {
   it("should can register new user", async () => {
     const result = await supertest(web).post("/api/users").send({
       username: "test",
-      password: "test",
+      password: "rahasia",
       name: "test",
     });
-
-    logger.info(result.body);
 
     expect(result.status).toBe(200);
     expect(result.body.data.username).toBe("test");
@@ -31,8 +28,6 @@ describe("POST /api/users", function () {
       name: "",
     });
 
-    logger.info(result.body);
-
     expect(result.status).toBe(400);
     expect(result.body.errors).toBeDefined();
   });
@@ -40,7 +35,7 @@ describe("POST /api/users", function () {
   it("should reject if username already registered", async () => {
     let result = await supertest(web).post("/api/users").send({
       username: "test",
-      password: "test",
+      password: "rahasia",
       name: "test",
     });
 
@@ -51,11 +46,9 @@ describe("POST /api/users", function () {
 
     result = await supertest(web).post("/api/users").send({
       username: "test",
-      password: "test",
+      password: "rahasia",
       name: "test",
     });
-
-    logger.info(result.body);
 
     expect(result.status).toBe(400);
     expect(result.body.errors).toBeDefined();
@@ -74,47 +67,39 @@ describe("POST /api/users/login", function () {
   it("should can login", async () => {
     const result = await supertest(web).post("/api/users/login").send({
       username: "test",
-      password: "test",
+      password: "rahasia",
     });
-
-    logger.info(result.body);
 
     expect(result.status).toBe(200);
     expect(result.body.data.token).toBeDefined();
     expect(result.body.data.token).not.toBe("test");
   });
 
-  it("should reject login if login is invalid", async () => {
+  it("should reject login if request is invalid", async () => {
     const result = await supertest(web).post("/api/users/login").send({
       username: "",
       password: "",
     });
 
-    logger.info(result.body);
-
     expect(result.status).toBe(400);
     expect(result.body.errors).toBeDefined();
   });
 
-  it("should reject login if password wrong", async () => {
+  it("should reject login if password is wrong", async () => {
     const result = await supertest(web).post("/api/users/login").send({
       username: "test",
-      password: "wrong",
+      password: "salah",
     });
-
-    logger.info(result.body);
 
     expect(result.status).toBe(401);
     expect(result.body.errors).toBeDefined();
   });
 
-  it("should reject login if username wrong", async () => {
+  it("should reject login if username is wrong", async () => {
     const result = await supertest(web).post("/api/users/login").send({
-      username: "wrong",
-      password: "test",
+      username: "salah",
+      password: "salah",
     });
-
-    logger.info(result.body);
 
     expect(result.status).toBe(401);
     expect(result.body.errors).toBeDefined();
@@ -130,19 +115,17 @@ describe("GET /api/users/current", function () {
     await removeTestUser();
   });
 
-  it("bisa mendapatkan users sekarang", async () => {
+  it("should can get current user", async () => {
     const result = await supertest(web)
       .get("/api/users/current")
       .set("Authorization", "test");
-
-    logger.info(result.body);
 
     expect(result.status).toBe(200);
     expect(result.body.data.username).toBe("test");
     expect(result.body.data.name).toBe("test");
   });
 
-  it("bisa menolak jika token tidak ada", async () => {
+  it("should reject if token is invalid", async () => {
     const result = await supertest(web)
       .get("/api/users/current")
       .set("Authorization", "salah");
@@ -161,71 +144,63 @@ describe("PATCH /api/users/current", function () {
     await removeTestUser();
   });
 
-  it("bisa update users", async () => {
+  it("should can update user", async () => {
     const result = await supertest(web)
       .patch("/api/users/current")
       .set("Authorization", "test")
       .send({
-        name: "Edwan",
-        password: "rahasia",
+        name: "Eko",
+        password: "rahasialagi",
       });
-
-    logger.info(result.body);
 
     expect(result.status).toBe(200);
     expect(result.body.data.username).toBe("test");
-    expect(result.body.data.name).toBe("Edwan");
+    expect(result.body.data.name).toBe("Eko");
 
     const user = await getTestUser();
-    expect(await bcrypt.compare("rahasia", user.password)).toBe(true);
+    expect(await bcrypt.compare("rahasialagi", user.password)).toBe(true);
   });
 
-  it("bisa update user name", async () => {
+  it("should can update user name", async () => {
     const result = await supertest(web)
       .patch("/api/users/current")
       .set("Authorization", "test")
       .send({
-        name: "Edwan",
+        name: "Eko",
       });
-
-    logger.info(result.body);
 
     expect(result.status).toBe(200);
     expect(result.body.data.username).toBe("test");
-    expect(result.body.data.name).toBe("Edwan");
+    expect(result.body.data.name).toBe("Eko");
   });
 
-  it("bisa update user password", async () => {
+  it("should can update user password", async () => {
     const result = await supertest(web)
       .patch("/api/users/current")
       .set("Authorization", "test")
       .send({
-        password: "rahasia",
+        password: "rahasialagi",
       });
-
-    logger.info(result.body);
 
     expect(result.status).toBe(200);
     expect(result.body.data.username).toBe("test");
     expect(result.body.data.name).toBe("test");
 
     const user = await getTestUser();
-    expect(await bcrypt.compare("rahasia", user.password)).toBe(true);
+    expect(await bcrypt.compare("rahasialagi", user.password)).toBe(true);
   });
 
-  it("bisa menolak akses jika request tidak valid", async () => {
+  it("should reject if request is not valid", async () => {
     const result = await supertest(web)
       .patch("/api/users/current")
       .set("Authorization", "salah")
       .send({});
 
-    logger.info(result.body);
-
     expect(result.status).toBe(401);
   });
 });
 
-describe("DELETE /api/users/current", function () {
+describe("DELETE /api/users/logout", function () {
   beforeEach(async () => {
     await createTestUser();
   });
@@ -234,7 +209,7 @@ describe("DELETE /api/users/current", function () {
     await removeTestUser();
   });
 
-  it("bisa logout", async () => {
+  it("should can logout", async () => {
     const result = await supertest(web)
       .delete("/api/users/logout")
       .set("Authorization", "test");
@@ -246,7 +221,7 @@ describe("DELETE /api/users/current", function () {
     expect(user.token).toBeNull();
   });
 
-  it("bisa menolak logout jika request tidak valid", async () => {
+  it("should reject logout if token is invalid", async () => {
     const result = await supertest(web)
       .delete("/api/users/logout")
       .set("Authorization", "salah");
